6317900508967b129ca9a8fa186dc67da3998206
Nejmeddine BEN OUARRED <nejmeddinebenouarred@MAC260.local>
2018-09-23 12:23:52 +0200

    Change context

---
+++ src/main/java/com/nej/perso/echange/config/WebServerConfig.java
@@ -0,0 +1,18 @@
+package com.nej.perso.echange.config;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class WebServerConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+    webServerFactoryCustomizer() {
+        return factory -> factory.setContextPath("/echange-scolaire/api");
+    }
+
+}
